0dd25fb7e2bb3dcaf38b916158a234b7159ab275
Leonardo Huttner <leonardohuttner@gmail.com>
2020-08-28 13:58:55 -0300

    Finalizado

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
-<script>
 
-        var app = new Vue({
-            el: "#app",
-            data: {
-                nome: '',
-                termo: false,
-                imc: null,
-                tabela: false,
-            },
-            methods: {
-                calculaIMC() {
-                    this.imc = (this.peso / (this.altura * this.altura)).toFixed(2)
-                    this.tabela = true
-                },
-            },
-        })
-    </script>
+var app = new Vue({
+    el: "#app",
+    data: {
+        nome: '',
+        termo: false,
+        imc: null,
+        tabela: false,
+    },
+    methods: {
+        calculaIMC() {
+            this.imc = (this.peso / (this.altura * this.altura)).toFixed(2)
+            this.tabela = true
+        },
+    },
+})
